157886e9b165e2aea2ec906a28e4bd997e430ff7
Bogdan Zvyagintsev <b.zvyagintsev@gmail.com>
2025-02-07 07:08:15 +0100

    fix: change placeholder

--- src/components/editor/no-content.tsx
+++ src/components/editor/no-content.tsx
@@ -3,7 +3,7 @@ export function NoContent() {
     <div className="col-span-6 row-span-2 flex items-center justify-center">
       <div className="flex h-1/2 w-4/5 flex-col items-center justify-center rounded-xl border-2 border-dashed">
         <p className="text-center text-muted-foreground">
-          Select an image to start editing
+          Choose photo to start editing
         </p>
       </div>
     </div>
